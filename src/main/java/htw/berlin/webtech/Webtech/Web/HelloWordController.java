d80feb734abd71bf3f23234c00b1ce4e3449fd42
Possidesiree <desipossi01@gmail.com>
2022-10-23 18:37:28 +0200

    Spring Boot Thymelaef/Zusammenführung Spring Boot mit dem Github_Repo

---
+++ src/main/java/htw/berlin/webtech/Webtech/Web/HelloWordController.java
@@ -0,0 +1,14 @@
+package htw.berlin.webtech.Webtech.Web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWordController {
+
+    @GetMapping(path="/")
+    public ModelAndView showHelloWorldPage(){
+        return new ModelAndView("helloworld");
+    }
+}
